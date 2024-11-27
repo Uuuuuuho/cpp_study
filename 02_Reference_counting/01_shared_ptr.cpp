3424de289de6b58471bb858b279322fab2731ff2
James_Ho <dbgh9129@gmail.com>
2024-11-27 08:28:30 +0000

    Shared pointer example

--- 02_Reference_counting/01_shared_ptr.cpp
+++ 02_Reference_counting/01_shared_ptr.cpp
@@ -5,6 +5,8 @@ class Truck{};
 
 int main(){
   std::shared_ptr<Truck> sp1(new Truck);
+  // shared pointer를 사용하면 인스턴스들이 가리키는 
+  // 객체는 동일하게 유지되니까 메모리 효율성이 좋아지게 됩니다.
   std::shared_ptr<Truck> sp2 = sp1;
 
   Truck* p1 = new Truck;
