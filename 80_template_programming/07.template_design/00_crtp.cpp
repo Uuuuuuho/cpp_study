bb88a81431055c4d248a7c6d0837198166282df8
Uuuuuuho <dbgh9129@gmail.com>
2024-12-02 01:01:16 -0800

    mistype correction

--- 80_template_programming/07.template_design/00_crtp.cpp
+++ 80_template_programming/07.template_design/00_crtp.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-/* CTRP (Curiously Recurring Template Pattern)
+/* CRTP (Curiously Recurring Template Pattern)
  *  "기반 클래스에서 파생 클래스의 클래스 이름"을 사용할 수 있게 하는 기술
  * CTRP 핵심
  * 1. "기반 클래스를 템플릿"으로 만들고
@@ -20,7 +20,7 @@ public:
         // 여기서 파생클래스(Derived) 이름을 사용할 수 없을까?
         // 파생클래스의 순서가 나중이기 때문에 알 수 없지만.
         // 즉, 어떤 파생클래스가 기반클래스를 호출할지 알 수 없지만.
-        // 이를 가능하게 해주는게 "CTRP"라는 기술임.
+        // 이를 가능하게 해주는게 "CRTP"라는 기술임.
         T obj; // Derived obj 
 
         std::cout << typeid(T).name() << std::endl;
